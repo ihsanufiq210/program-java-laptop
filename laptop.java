3284c53a3f23350f1886ce48536445589b11cd08
ihsanufiq210 <ihsanufik175@gmail.com>
2025-01-15 14:19:50 +0700

    Add files via upload

---
+++ laptop.java
@@ -0,0 +1,14 @@
+public class laptop {
+    String namalaptop = "lenovo";
+    String warnalaptop = "biru";
+    String jenislaptop = "ideapad";
+    int laptop;
+
+    void tampillaptop(){
+        System.out.println("namalaptop = " + namalaptop);
+        System.out.println("warnalaptop =" + warnalaptop); 
+        System.out.println("jenislaptop =" + jenislaptop);
+    }
+
+    
+}
---
+++ objeklaptop.java
@@ -0,0 +1,8 @@
+public class objeklaptop {
+    
+    public static void main(String[] args) {
+        laptop l = new laptop();
+        l.tampillaptop();
+
+    }
+}
